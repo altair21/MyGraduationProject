a45eeb5f2ac40be9973d1101920d0cf1e1dd690c
altair21 <lovealtair21@gmail.com>
2016-06-01 16:19:02 +0800

    smoothly UI

--- App/GraduationProject/GraduationProject/GameListTableViewCell.swift
+++ App/GraduationProject/GraduationProject/GameListTableViewCell.swift
@@ -15,9 +15,8 @@ class GameListTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
-
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
@@ -31,58 +30,67 @@ class GameListTableViewCell: UITableViewCell {
             _filePath = filePath
         }
         dispatch_async(dispatch_get_main_queue()) {
+            if self.previewZone == nil {
 //            self.layer.borderColor = UIColor.blueColor().CGColor
 //            self.layer.borderWidth = 2.0
-            self.previewZone = UIView(frame: CGRect(x: 40, y: 0, width: 640, height: 440))
+                self.previewZone = UIView(frame: CGRect(x: 40, y: 0, width: 640, height: 440))
 //            previewZone.layer.shadowColor = UIColor.blackColor().CGColor
 //            previewZone.layer.shadowOffset = CGSize(width: 4, height: 4)
 //            previewZone.layer.shadowOpacity = 0.8
+                self.BGView.addSubview(self.previewZone)
+            } else {
+                for view in self.previewZone.subviews {
+                    view.removeFromSuperview()
+                }
+            }
             let previewZoneBG = UIImageView(image: UIImage(named: "background"))
             previewZoneBG.frame = CGRect(x: 0, y: 0, width: 640, height: 440)
             self.previewZone.addSubview(previewZoneBG)
-            self.BGView.addSubview(self.previewZone)
-            
-            self.loadLevel(filePath)
         }
+        self.loadLevel(filePath)
     }
     
     func loadLevel(filePath: String) {
-        if let levelString = try? String(contentsOfFile: filePath, usedEncoding: nil) {
-            let lines = levelString.componentsSeparatedByString("\n")
-            
-            for (row, line) in lines.reverse().enumerate() {
-                for (column, letter) in line.characters.enumerate() {
-                    let position = CGPoint(x: vPreviewTextureLength * column,
-                                           y: vPreviewTextureLength * (21 - row))
-                    
-                    let texture = UIImageView()
-                    texture.frame = CGRect(x: position.x,
-                                           y: position.y,
-                                           width: CGFloat(vPreviewTextureLength),
-                                           height: CGFloat(vPreviewTextureLength))
-                    switch letter {
-                    case "1":
-                        texture.image = UIImage(named: "spring_node_1")
-                    case "2":
-                        texture.image = UIImage(named: "spring_node_2")
-                    case "3":
-                        texture.image = UIImage(named: "spring_node_3")
-                    case "4":
-                        texture.image = UIImage(named: "spring_node_4")
-                    case "x":
-                        texture.image = UIImage(named: "block")
-                    case "v":
-                        texture.image = UIImage(named: "vortex")
-                    case "s":
-                        texture.image = UIImage(named: "star")
-                    case "f":
-                        texture.image = UIImage(named: "finish")
-                    case "p":
-                        texture.image = UIImage(named: "player")
-                    default: break
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) { 
+            if let levelString = try? String(contentsOfFile: filePath, usedEncoding: nil) {
+                let lines = levelString.componentsSeparatedByString("\n")
+                
+                for (row, line) in lines.reverse().enumerate() {
+                    for (column, letter) in line.characters.enumerate() {
+                        let position = CGPoint(x: vPreviewTextureLength * column,
+                                               y: vPreviewTextureLength * (21 - row))
                         
+                        let texture = UIImageView()
+                        texture.frame = CGRect(x: position.x,
+                                               y: position.y,
+                                               width: CGFloat(vPreviewTextureLength),
+                                               height: CGFloat(vPreviewTextureLength))
+                        switch letter {
+                        case "1":
+                            texture.image = UIImage(named: "spring_node_1")
+                        case "2":
+                            texture.image = UIImage(named: "spring_node_2")
+                        case "3":
+                            texture.image = UIImage(named: "spring_node_3")
+                        case "4":
+                            texture.image = UIImage(named: "spring_node_4")
+                        case "x":
+                            texture.image = UIImage(named: "block")
+                        case "v":
+                            texture.image = UIImage(named: "vortex")
+                        case "s":
+                            texture.image = UIImage(named: "star")
+                        case "f":
+                            texture.image = UIImage(named: "finish")
+                        case "p":
+                            texture.image = UIImage(named: "player")
+                        default: break
+                            
+                        }
+                        dispatch_async(dispatch_get_main_queue(), { 
+                            self.previewZone.addSubview(texture)
+                        })
                     }
-                    self.previewZone.addSubview(texture)
                 }
             }
         }
